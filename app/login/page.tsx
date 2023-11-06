0b8feb50271f448283bb97b7e34c76606772cafa
LoicAvelin <loic.avelin@protonmail.com>
2023-11-06 12:05:22 +0100

    First commit to the project nextjs-dashboard

---
+++ app/login/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <h1 className='justify-center flex'>Ceci est ma page login</h1>
+    )
+}
---
+++ app/metre/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <h1>Ceci est une magnifique page</h1>
+    )
+}
